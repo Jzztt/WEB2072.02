a9b8a6ff09adfcd717625881169b7555ff44ea2a
Jettsyy <teazyyy2512@gmail.com>
2025-11-25 15:11:34 +0700

    feat: slot and provide inject

--- src/components/ChildrenComponent.vue
+++ src/components/ChildrenComponent.vue
@@ -1,21 +1,32 @@
 <template>
   <div style="background-color: blue">
     <h2>children Component</h2>
+    <slot name="header"></slot>
     {{ message }}
 
+    <!-- Bước 1: Khi sự kiện click được kích hoạt => handleClick được chạy -->
     <button @click="handleClick">Click</button>
+    <slot name="footer"></slot>
+    {{ noti }}
   </div>
 </template>
 
 <script setup>
+import { inject } from 'vue'
+
+// định nghĩa biến nhận giá triwj từ cha
 defineProps({
   message: String,
 })
+// định nghĩa sự kiện nhận vào từ cha
 const emit = defineEmits(['notify'])
 const handleClick = () => {
   // alert('Button in Children Component clicked!')
+  // emit dùng kích hoạt sự kiện
   emit('notify', 'Button in Children Component clicked! aaaaa')
 }
+// inject lấy giá trị có key là noti được cung cấp từ provide nếu k có lấy tham số thứ 2
+const noti = inject('noti', 'No notification')
 </script>
 
 <style lang="scss" scoped></style>
--- src/components/ParentComponent.vue
+++ src/components/ParentComponent.vue
@@ -4,15 +4,22 @@
       <h2>Parent Component</h2>
       <p>This is the parent component.</p>
     </div>
-    <ChildrenComponent :message="msg" @notify="handleChildEvent" />
+    <ChildrenComponent :message="msg" @notify="handleChildEvent">
+      <template v-slot:header> Đây là header </template>
+      <template v-slot:footer> Đây là Footer </template>
+    </ChildrenComponent>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { provide, ref } from 'vue'
 import ChildrenComponent from './ChildrenComponent.vue'
 const msg = ref('Hello')
 const name = ref('Vue3')
+
+const notification = ref('Parent call children')
+
+provide('notis', notification.value)
 
 const handleChildEvent = (msg) => {
   alert(msg)
